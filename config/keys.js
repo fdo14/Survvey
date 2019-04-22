e72986e0ace49fda2119f4059900f5311654c14b
terrysducks <41840392+terrysducks@users.noreply.github.com>
2019-04-22 16:35:03 -0400

    changes

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "538282421443-jg6hi3stt5m1utog308n2jmgv5tskg29.apps.googleusercontent.com",
+  googleClientSecret: "jGUqoe0lXWK-ebVd_GQrViCl",
+  mongoURI: "mongodb://fdo14:password1@ds031561.mlab.com:31561/survvey",
+  cookieKey: "asdkjfhaygfwqfwghjsnvjsdgfdyqweifweufghasjdf"
+};
