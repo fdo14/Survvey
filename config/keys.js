dc4c752adad75595711016a7bcd179f2985be890
terrysducks <41840392+terrysducks@users.noreply.github.com>
2019-04-23 16:28:33 -0400

    heroku build

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,7 @@ module.exports = {
     "538282421443-jg6hi3stt5m1utog308n2jmgv5tskg29.apps.googleusercontent.com",
   googleClientSecret: "jGUqoe0lXWK-ebVd_GQrViCl",
   mongoURI: "mongodb://fdo14:password1@ds031561.mlab.com:31561/survvey",
-  cookieKey: "asdkjfhaygfwqfwghjsnvjsdgfdyqweifweufghasjdf"
+  cookieKey: "asdkjfhaygfwqfwghjsnvjsdgfdyqweifweufghasjdf",
+  stripePublishableKey: "pk_test_rAvZ9ItVl80Ny0XcLuDmDa5D00FS4oq8dF",
+  stripeSecretKey: "sk_test_CM2OAUn9wSXJPDMPMHJJbVd2007H3xDk0v"
 };
